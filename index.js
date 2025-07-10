b5d796465e26ba750dc68f1d69b4f1dc8faf75b3
emad-pa <emadpa82@gmail.com>
2025-07-11 01:38:22 +0530

    index.js modified

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const flash=require("connect-flash");
 const passport=require("passport");
 const LocalStrategy=require("passport-local");
 const User=require("./models/user.js");
-const sampleListings=require("./data.js");
+
 
 
 

a094a49f1f8f5bacc7e16395945e96f8cbe9ad83
emad-pa <emadpa82@gmail.com>
2026-01-16 14:36:34 +0530

    Fix ExpressError import path

--- index.js
+++ index.js
@@ -1,78 +1,63 @@
-const express=require("express");
-const app=express();
-const mongoose=require("mongoose");
-const List=require("./models/lists.js");
-const path=require("path");
-const port=5000;
-const methodOverride=require("method-override");
-const  ejsmate = require("ejs-mate");
-const listroute=require("./routes/list.js");
-const reviewroute=require("./routes/review.js");
-const bookingroute=require("./routes/book.js");
-const userroute=require("./routes/user.js");
-const session=require("express-session");
-const MongoStore = require('connect-mongo');
-const flash=require("connect-flash");
-const passport=require("passport");
-const LocalStrategy=require("passport-local");
-const User=require("./models/user.js");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const List = require("./models/lists.js");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsmate = require("ejs-mate");
+const listroute = require("./routes/list.js");
+const reviewroute = require("./routes/review.js");
+const bookingroute = require("./routes/book.js");
+const userroute = require("./routes/user.js");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
-
-
-
-if(process.env.NODE_ENV != "production"){
-
-   require('dotenv').config();
-
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config();
 }
 
-
-
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(express.json());
-app.set("views",path.join(__dirname,"views"));
-app.use(express.urlencoded({extended: true }));
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("__method"));
-app.engine('ejs', ejsmate);
-app.use(express.static(path.join(__dirname,"/public")));
+app.engine("ejs", ejsmate);
+app.use(express.static(path.join(__dirname, "/public")));
 
-
-
-main().then((res)=>{                 
+main()
+  .then((res) => {
     console.log("db connected");
-}).catch((err)=>{
-   console.log(err);
-})
-async function main(){
-
-   await mongoose.connect(process.env.ATLAS_URL);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+async function main() {
+  await mongoose.connect(process.env.ATLAS_URL);
 }
 
+const store = MongoStore.create({
+  mongoUrl: process.env.ATLAS_URL,
+  crypto: {
+    secret: process.env.SECRET,
+  },
+  touchAfter: 24 * 3600,
+});
 
-
-const store=MongoStore.create({ 
-    mongoUrl: process.env.ATLAS_URL ,
-    crypto:{
-       secret: process.env.SECRET,
-    },
-    touchAfter:24*3600,
-
-
-}
-)
-
-const sessionoptions={
-      store,
-      secret: process.env.SECRET ,
-      resave:false,
-      saveUninitialized:false,
-      cookie:{
-        expires:new Date( Date.now()+7*24*60*60*1000),
-        maxAge:7*24*60*60*1000,
-        httpOnly:true
-      }
-}
-
+const sessionoptions = {
+  store,
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  },
+};
 
 app.use(session(sessionoptions));
 app.use(flash());
@@ -82,44 +67,34 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use((req,res,next)=>{
-    res.locals.authenticated=req.isAuthenticated();
-    res.locals.success=req.flash("success");
-    res.locals.error=req.flash("error");
-    res.locals.curuser=req.user;
-    next();
-
+app.use((req, res, next) => {
+  res.locals.authenticated = req.isAuthenticated();
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  res.locals.curuser = req.user;
+  next();
 });
 
+app.use("/api/list", listroute);
+app.use("/api/list/:id/review/", reviewroute);
+app.use("/api/user", userroute);
+app.use("/api/list/book", bookingroute);
 
-app.use("/api/list",listroute);
-app.use("/api/list/:id/review/",reviewroute);
-app.use("/api/user",userroute);
-app.use("/api/list/book",bookingroute);
-
-
-
-app.use("/",async (req,res)=>{
-      let listings=await List.find({});
-     return res.render("listings/lists",{listings});
+app.get("/", async (req, res) => {
+  let listings = await List.find({});
+  res.render("listings/lists", { listings });
 });
 
+app.use((err, req, res, next) => {
+  if (res.headersSent) {
+    return next(err); // Avoid double responses
+  }
+  let { status = 500, message = "Something went wrong" } = err;
 
-
-app.use((err,req,res,next)=>{
-       if (res.headersSent) {
-        return next(err); // Avoid double responses
-    }    
-   let { status =500, message = "Something went wrong" }=err;
-  
-  return res.render("error/error.ejs",{message});
-   
-
+  return res.render("error/error.ejs", { message });
 });
-
-
-
-
-app.listen(port,()=>{    //server started
-    console.log("server started");
-})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  //server started
+  console.log("server started");
+});
--- middleware.js
+++ middleware.js
@@ -1,61 +1,62 @@
-const List=require("./models/lists");
-const Review=require("./models/review");
-const {listingschema}=require("./schemavalidation.js");
+const List = require("./models/lists");
+const Review = require("./models/review");
+const { listingschema } = require("./schemavalidation.js");
+const ExpressError = require("./utils/customerror.js");
 
-const isloggedin=(req,res,next)=>{
-     if(!req.isAuthenticated()){
-      req.session.redirectUrl=req.originalUrl;  
-        req.flash("error","you must login ");
-        return res.redirect("/api/user/login");
-    }
-    next();
+const isloggedin = (req, res, next) => {
+  if (!req.isAuthenticated()) {
+    req.session.redirectUrl = req.originalUrl;
+    req.flash("error", "you must login ");
+    return res.redirect("/api/user/login");
+  }
+  next();
+};
+const saveredirecturl = (req, res, next) => {
+  if (req.session.redirectUrl) {
+    res.locals.redirectUrl = req.session.redirectUrl;
+  }
+  next();
+};
 
-}
-const saveredirecturl=(req,res,next)=>{
-    if( req.session.redirectUrl){
-        res.locals.redirectUrl=req.session.redirectUrl;
-    }
-    next();
+const validatelist = (req, res, next) => {
+  let result = listingschema.validate(req.body, { abortEarly: false });
+  if (result.error) {
+    throw new ExpressError(
+      400,
+      result.error.details.map((el) => el.message).join(", ")
+    );
+  }
+  next();
+};
 
-}
+const isowner = async (req, res, next) => {
+  const { id } = req.params;
+  const list = await List.findById(id);
 
-const validatelist=(req,res,next)=>{
+  if (list.owner.toString() != req.user._id) {
+    req.flash("error", "you are not the owner of property");
+    return res.redirect(`/api/list/${id}`);
+  }
 
-      let result=listingschema.validate(req.body, { abortEarly: false });
-    if(result.error){
-        throw new ExpressError(400,result.error.details.map(el => el.message).join(", "));
-    }
-    next();
-}
+  next();
+};
 
-const isowner=async (req,res,next)=>{
-  
-     const {id}=req.params;
-     const list=await List.findById(id);
+const isreviewauthor = async (req, res, next) => {
+  const { id, reviewid } = req.params;
+  const review = await Review.findById(reviewid);
 
-     if(list.owner.toString() !=req.user._id){
-             
-         req.flash("error","you are not the owner of property");
-         return res.redirect(`/api/list/${id}`);
-     }
+  if (review.author.toString() != req.user._id) {
+    req.flash("error", "you are not the author of this review");
+    return res.redirect(`/api/list/${id}`);
+  }
 
-     next();
+  next();
+};
 
-}
-
-const isreviewauthor=async (req,res,next)=>{
-  
-     const {id,reviewid}=req.params;
-     const review=await Review.findById(reviewid);
-
-     if(review.author.toString() != req.user._id){
-             
-         req.flash("error","you are not the author of this review");
-         return res.redirect(`/api/list/${id}`);
-     }
-
-     next();
-
-}
-
-module.exports={isloggedin,saveredirecturl,validatelist,isowner,isreviewauthor};
+module.exports = {
+  isloggedin,
+  saveredirecturl,
+  validatelist,
+  isowner,
+  isreviewauthor,
+};
